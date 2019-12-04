cd9c3f834257a6b2507470c18e53d6a04e694858
Kayden Chan <kaychan0526@gmail.com>
2019-12-03 17:08:16 -0800

    First

---
+++ Assets/Scenes/CameraView.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraView : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		Vector3 v = player.transform.position;
+		v.z = -10;
+		transform.position = v;
+    }
+}
